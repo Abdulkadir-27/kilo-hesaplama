da40001e1cb4e51452108c459783fdac1464126b
Abdulkadir-27 <kadiralici91@gmail.com>
2025-09-15 11:03:53 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let dogumyılı = prompt("doğum yılınızı giriniz:");
+let şuankiyıl = prompt("şu anki yılı giriniz:");
+if (dogumyılı - şuankiyıl ) {
+    alert(dogumyılı - şuankiyıl);
+}
+//! kilo hesaplama
+let kilo = Number (prompt("kilonuzu giriniz:"));
+let boy = Number (prompt("boyunuzu giriniz:"));
+let sonuc = kilo / (boy*2);
+if (sonuc < 18.5) {
+    alert("zayıf");
+} else if (sonuc < 24.9) {
+    alert("normal");
+} else {
+    alert("şişman");
+}
